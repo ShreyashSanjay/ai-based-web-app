a780eea4f967e5992ff9a5b5dd2a10effb495217
Shreyash <shreyash6@hotmail.co.uk>
2025-04-12 12:27:09 -0400

    added web deployment using flask

---
+++ server.py
@@ -0,0 +1,37 @@
+#api request with appropriate response
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    #get the text from the arguments of the response
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    #Get the relavent data from the application created and display each result
+    response = emotion_detector(text_to_analyze)
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    #edge case incase no emotion
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!."
+    return (
+        f"For the given statement, the system response is 'anger':"
+        f"{anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. " 
+        f"The dominant emotion is {dominant_emotion}."
+    )
+
+#Method renders the wanted
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
